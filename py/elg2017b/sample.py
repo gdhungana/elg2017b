ca4493adf39ebfdf716364067857d2d877093726
gdhungana <gdhungana100@gmail.com>
2017-09-18 23:18:20 -0500

    Add position to param and activate breakbricks

--- py/elg2017b/sample.py
+++ py/elg2017b/sample.py
@@ -24,23 +24,28 @@ def sample_airmass(expfile,nobs=5000,seed=1234):
     return sample
 
 
-def sample_ebv(expfile,nobs=5000,seed=1234):
+def sample_tiles(expfile,nobs=5000,seed=1234,ebv=False,factor=10):
     #- sample the tileid, and get the E(B-V) from desi-tiles.fits file
     import os
     rst=np.random.RandomState(seed)
     exp=fits.open(expfile)
     tileid=exp[1].data['tileid']
-    ii=rst.choice(len(tileid),size=nobs)
+    ra=exp[1].data['ra']
+    dec=exp[1].data['dec']
+    nn=nobs/factor#+nobs%factor #- limit to few bricks to avoid a single brick per brick
+    ii=rst.choice(len(tileid),size=nn)#- sampling only nobs/10 tiles
     sample_tiles=tileid[ii]
-
     #- find the match in desitiles 
     desitiles=fits.open(os.environ["DESIMODEL"]+'/data/footprint/desi-tiles.fits')
     dtileid=desitiles[1].data['TILEID']
     match=np.in1d(dtileid,sample_tiles)
-    ebv=desitiles[1].data['EBV_MED'][match]
-
-    return ebv
-
+    sample_ra=desitiles[1].data['RA'][match]
+    sample_dec=desitiles[1].data['DEC'][match]
+    sample_ebv=desitiles[1].data['EBV_MED'][match]
+    if ebv:
+        return np.tile(sample_ra,factor),np.tile(sample_dec,factor),np.tile(sample_ebv,factor)
+    else:
+        return np.tile(sample_ra,factor),np.tile(sample_dec,factor)
 
 def sample_moon(expfile,nobs=5000,seed=1234):
     #- only for gray time
